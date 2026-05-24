4a7aab5a5e1b3d2f878383143b3e979d6bf32afc
Andrea <andrealicheripakunza@gmail.com>
2026-05-24 12:30:17 -0400

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,10 @@ export default defineConfig({
         isr: {
             expiration: 60 * 60 * 24 * 30 * 6,
         }
-    })
+    }),
+    vite: {
+        optimizeDeps: {
+            exclude: ['astro'] // Prevents Bun from pre-bundling Astro's virtual internals
+        }
+    }
 });
